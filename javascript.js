83ecb77b9e08a66dcf6bc932d1413af1787a4715
TJ Wessel <twessel01@gmail.com>
2022-03-17 12:59:57 -0600

    Create function playRound()

--- javascript.js
+++ javascript.js
@@ -2,20 +2,70 @@
 // Computer will choose a random number between 0-2 that will correspond
 // to Rock, Paper, or Scissors. This will then be assigned to a string.
 
-let computerSelection
 
 function computerPlay() {
     let randNum = Math.floor(Math.random()*3);
-    console.log(randNum);
+    let computerSelection;
 
     if (randNum === 0) {
-        computerSelection = 'Rock';
+        computerSelection = 'rock';
     }
     else if (randNum === 1) {
-        computerSelection = 'Paper';
+        computerSelection = 'paper';
     }
     else if (randNum === 2) {
-        computerSelection = 'Scissors';
+        computerSelection = 'scissors';
     }
-    console.log(computerSelection);
+    return computerSelection
+}
+
+// playRound will take playerSelection as input, generate the computer's choice, and return a message delcaring the winner
+
+function playRound(playerSelection) {
+    const computerSelection = computerPlay();
+    playerSelection = playerSelection.toLowerCase();
+    let winnerMsg = '';
+
+    //There are 9 possible outcomes for Rock, Paper, Scissors. Nested if statements should do the trick.
+    //Consider updating this at some point with logic that recognizes the heirarchy of rock, paper and scissors.
+    //How could this expand more easily to include logic for more options?
+
+    if (playerSelection == 'rock') {
+        if (computerSelection == 'scissors') {
+            winnerMsg = 'You win! Rock beats scissors.';
+        }
+        else if (computerSelection == 'paper') {
+            winnerMsg = 'You lose! Paper beats rock.';
+        }
+        else if (computerSelection == 'rock'){
+            winnerMsg = 'Tie! You both picked rock.';
+        }
+    }
+    else if (playerSelection == 'paper') {
+        if (computerSelection == 'scissors') {
+            winnerMsg = 'You lose! Scissors beats paper.';
+        }
+        else if (computerSelection == 'paper') {
+            winnerMsg = 'Tie! You both picked paper.';
+        }
+        else if (computerSelection == 'rock'){
+            winnerMsg = 'You win! Paper beats rock.';
+        }
+    }
+    else if (playerSelection == 'scissors') {
+        if (computerSelection == 'scissors') {
+            winnerMsg = 'Tie! You both picked scissors.';
+        }
+        else if (computerSelection == 'paper') {
+            winnerMsg = 'You win! Scissors beats paper';
+        }
+        else if (computerSelection == 'rock'){
+            winnerMsg = 'You lose! Rock beats scissors';
+        }
+    }
+    else {
+        console.log("Please use 'Rock', 'Paper', or 'Scissor'");
+    }
+    return winnerMsg
+
 }
